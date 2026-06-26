cef91f7b140efb9ad498eb017781b357f3d69e5f
sky <sky-devops@liongaming.co>
2026-06-26 19:43:08 +0800

    客服调整

---
+++ services/chat/internal/logic/getchatuserbyidlogic.go
@@ -0,0 +1,38 @@
+package logic
+
+import (
+	"context"
+
+	"wklive/common/helper"
+	"wklive/proto/chat"
+	"wklive/services/chat/internal/svc"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type GetChatUserByIdLogic struct {
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+	logx.Logger
+}
+
+func NewGetChatUserByIdLogic(ctx context.Context, svcCtx *svc.ServiceContext) *GetChatUserByIdLogic {
+	return &GetChatUserByIdLogic{
+		ctx:    ctx,
+		svcCtx: svcCtx,
+		Logger: logx.WithContext(ctx),
+	}
+}
+
+// 获取用户
+func (l *GetChatUserByIdLogic) GetChatUserById(in *chat.GetChatUserByIdReq) (*chat.GetChatUserByIdResp, error) {
+	user, err := l.svcCtx.ChatUserModel.FindOne(l.ctx, in.Id)
+	if err != nil {
+		return &chat.GetChatUserByIdResp{Base: helper.FailResp()}, nil
+	} else {
+		return &chat.GetChatUserByIdResp{
+			Base: helper.OkResp(),
+			Data: toProtoUser(user),
+		}, nil
+	}
+}
